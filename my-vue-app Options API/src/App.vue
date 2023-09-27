651ce5ea1b9aa29a07a2b0646b5101940b0eb566
joao <joaosb004@gmail.com>
2023-09-26 21:41:01 -0300

    VueVsReact

---
+++ my-vue-app Options API/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <p id="p1">You clicked {{ numOfClicks }} times.</p>
+    <button id="btn1" @click="clickCount()">Add +1</button>
+    <button id="btn2" @click="clearAll">Reset</button>
+  </div>
+</template>
+
+<script>
+console.log("Options API RENDER");
+export default {
+  name: "App",
+  data() {
+    return { numOfClicks: 0 };
+  },
+  created() {},
+  methods: {
+    clickCount: function () {
+      this.numOfClicks++;
+    },
+    clearAll: function () {
+      this.numOfClicks = 0;
+    },
+  },
+};
+</script>
+
+<style>
+@import "./style.css";
+</style>
---
+++ my-vue-app Options API/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+console.log("Options API RENDER 2");
+const app = createApp(App);
+app.mount("#app");
